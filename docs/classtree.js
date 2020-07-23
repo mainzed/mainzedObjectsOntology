c66638ce4737c6cbb521ef48b8b3af12bf4b2b97
timo.homburg <timo.homburg@hs-mainz.de>
2020-07-23 13:18:29 +0200

    Classtree update

--- docs/classtree.js
+++ docs/classtree.js
@@ -4,32 +4,32 @@ function generateClassTree(titleatt,superatt){
 	classTree["core"]["data"].push({ "id" : "http://www.opengis.net/ont/geosparql#Feature", "parent":"#", "text" : "Feature" })
 	classTree["core"]["data"].push({ "id" : "Enumeration", "parent":"http://www.opengis.net/ont/geosparql#Feature", "text" : "Enumeration" })
 	var counter=0;
-	console.log($('#ontview').contents())
-	console.log($('#ontview').contents().find('.type-c'))
-	console.log($('#ontview').contents().find(' h3 > sup[title="class"]'))
+	//console.log($('#ontview').contents())
+	//console.log($('#ontview').contents().find('.type-c'))
+	//console.log($('#ontview').contents().find(' h3 > sup[title="class"]'))
 	$('#ontview').contents().find(' h3 > sup[title="'+titleatt+'"]').each(function() {
-		//console.log($(this))
+		////console.log($(this))
 		if(counter>0){
 		var id=$(this).parent().parent().attr("id");
-		//console.log(id)
+		////console.log(id)
 		var parentcls="";
-		//console.log("Superclasses")
+		////console.log("Superclasses")
 		if(!(id.startsWith("4"))){
 			sup=$(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a")
 			if(sup.length!=0){
 			$(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a").each(function(){
-			console.log($(this))
+			//console.log($(this))
 			if(!($(this).attr("href").startsWith("4"))){
 				parentcls=$(this).attr("href").substring($(this).attr("href").indexOf('#')+1)
-				//console.log($(this).attr("href"));
+				////console.log($(this).attr("href"));
 			}
 		});
 			}else{
 					$(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().children("a").each(function(){
-			console.log($(this))
+			//console.log($(this))
 			if(!($(this).attr("href").startsWith("4"))){
 				parentcls=$(this).attr("href").substring($(this).attr("href").indexOf('#')+1)
-				//console.log($(this).attr("href"));
+				////console.log($(this).attr("href"));
 			}
 		});
 			}
@@ -38,15 +38,15 @@ function generateClassTree(titleatt,superatt){
 			parentcls="#"
 		}
 		
-		//console.log(superclasslist[0])
+		////console.log(superclasslist[0])
 		//var topush={ "id" : id,parent:
 		classTree["core"]["data"].push({ "id" : id, "parent":parentcls, "text" : id.substring(id.lastIndexOf('#')+1) })
-		console.log(classTree["core"]["data"])
+		//console.log(classTree["core"]["data"])
 		}
 		}
 		counter++;
 		
 	});
-	console.log(classTree)
+	//console.log(classTree)
 	return classTree;
 }
