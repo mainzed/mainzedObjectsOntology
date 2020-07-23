936bbb82843abe0877fd583015fcfbfb9ac0bcba
Timo Homburg <timo.homburg@hs-mainz.de>
2020-07-24 01:38:44 +0200

    Update classtree.js

--- docs/classtree.js
+++ docs/classtree.js
@@ -19,6 +19,7 @@ function generateClassTree(titleatt,superatt,classOrProp){
 		if(!(id.startsWith("4")) && $(this).parent().parent().children('dl').length>0 
             && $(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').length>0
             && $(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().length>0
+            && $(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a").length>0
         ){
 			sup=$(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a")
 			if(sup.length!=0){
@@ -31,7 +32,9 @@ function generateClassTree(titleatt,superatt,classOrProp){
 		});
 			}else if($(this).parent().parent().children('div').children('dl').length>0 
                 && $(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').length>0
-                && $(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().length>0){
+                && $(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().length>0
+                && $(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().children("a").length>0
+                ){
 					$(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().children("a").each(function(){
                             console.log($(this))
                     if(!($(this).attr("href").startsWith("4"))){
